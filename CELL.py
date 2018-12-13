5baba1ef99ded3440a50dd5dd7e80c737263878b
Alex Liebscher <alexliebscher0@gmail.com>
2018-12-13 14:24:24 -0800

    CELL data set complete

---
+++ CELL.py
@@ -0,0 +1,152 @@
+import numpy as np
+import os
+
+import datetime, time
+import warnings
+import multiprocessing as mp
+from multiprocessing import Pool
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+import xgboost as xgb
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+
+from skopt import BayesSearchCV
+from skopt.space import Real, Integer
+
+from sklearn.model_selection import KFold, train_test_split
+
+print('Libraries loaded')
+
+CELL_data = np.load('/Volumes/microSD/COGS118a/CELL_data.npy')
+CELL_target = np.load('/Volumes/microSD/COGS118a/CELL_target.npy')
+
+print(f'CELL shape: {CELL_data.shape}')
+
+samples, features = CELL_data.shape
+        
+def log(message):
+    t = datetime.datetime.now()
+    
+    with open('CELL.log', 'a') as logf:
+        if isinstance(message, str):
+            logf.write(f'[{t}] {message}\n')
+        elif isinstance(message, list):
+            for i, m in enumerate(message):
+                logf.write(f'[{t}] ({i}) {m}\n')
+        
+
+def log_results(partition, classifier, training, validation, testing, params, time_delta):
+    
+    dur = str(datetime.timedelta(seconds=time_delta))
+    
+    res = []
+    for key in params:
+        res.append(f'{key}: {params[key]}')
+        
+    log([f'RESULTS [{dur}]: clf: {classifier}, p: {partition}, train: {training:.4f}, val: {validation:.4f}, test: {testing:.4f}',
+         f'BEST_PARAMS: {", ".join(res)}'])
+
+# Initialize KFold    
+
+folds = 5
+
+kf = KFold(n_splits=folds, shuffle=True)
+
+# Initialize Classifiers
+
+models = [
+    ('xgb', xgb.XGBClassifier()),
+    ('rfc', RandomForestClassifier(n_estimators=1024, n_jobs=-1, criterion='entropy')),
+    ('ada', AdaBoostClassifier()),
+    ('svc_lin', LinearSVC()),
+    ('svc_rbf', SVC(kernel='rbf')),
+    ('knn', KNeighborsClassifier(n_jobs=-1)),
+    ('logreg', LogisticRegression(penalty='l2', n_jobs=-1)),
+]
+
+param_space = [
+    ({'n_estimators': Integer(2, 1024)}, 20),
+    ({'max_features': Integer(1, features-1)}, 24),
+    ({'n_estimators': Integer(2, 1024), 'learning_rate': Real(1e-6, 100, prior='log-uniform')}, 20),
+    ({'C': Real(1e-7, 1000, prior='log-uniform')}, 20),
+    ({'gamma': Real(0.001, 2, prior='log-uniform'), 'C': Real(1e-7, 1000, prior='log-uniform')}, 12),
+    ({'weights': ['uniform', 'distance']}, 16),
+    ({'C': Real(1e-8, 1e+4, prior='log-uniform')}, 16),
+]
+
+trials = 3
+partitions = [0.5, 0.8]
+early_stop_tol = 1e-4
+stop_after_iters = 7
+
+def train_val(classifier, name, partition, trial, params, n_iter, X_train, y_train):
+    bscv = BayesSearchCV(classifier, params, n_iter=n_iter, cv=kf, scoring='f1', return_train_score=True, n_jobs=-1)
+        
+    log(f'Making {bscv.total_iterations} iterations on {name}_{trial+1} ({partition})')
+    
+    total_iters = [0]
+    prior_scores = []
+    
+    def on_step(optim_result):
+        total_iters[0] += 1
+        prior_scores.append(bscv.best_score_)
+        
+        log(f'{name}{total_iters}[{trial+1}] current best score: {bscv.best_score_:.4f}')
+        
+        if total_iters[0] >= stop_after_iters:
+            if bscv.best_score_ == 1.0 or (np.mean(prior_scores[-stop_after_iters:]) - np.mean(prior_scores[-(stop_after_iters+1):-1])) < early_stop_tol:
+                log(f'{name}{total_iters}[{trial+1}] stopped early')
+                return True
+    
+    bscv.fit(X_train, y_train, callback=on_step)
+        
+    return bscv
+
+def test(estimator, X_test, y_test):
+    return estimator.score(X_test, y_test)
+
+def run_trial(trial_num):
+    
+    for partition in partitions:
+        
+        # since random can't play nicely with multiprocessing
+        # https://stackoverflow.com/questions/24345637/why-doesnt-numpy-random-and-multiprocessing-play-nice
+        seed = int.from_bytes(os.urandom(4), byteorder='little')
+        
+        X_train_val, X_test, y_train_val, y_test = train_test_split(CELL_data, CELL_target, test_size=(1-partition), random_state=seed)
+        
+        split = len(X_train_val)
+                
+        log(f'TRIAL #{trial_num+1}, TRAIN SZ: {split}, TEST SZ: {samples-split}')
+
+        for (name, clf), (params, n_iter) in zip(models, param_space):
+            
+            if name == 'knn':
+                params['n_neighbors'] = Integer(1,int(split*(folds-1)/folds))
+                            
+            start_time = time.time()
+            
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                
+                gs = train_val(clf, name, partition, trial_num, params, n_iter, X_train_val, y_train_val)
+                t_score = test(gs.best_estimator_, X_test, y_test)
+                
+            end_time = time.time()
+
+            log_results(partition, name, 
+                        max(gs.cv_results_['mean_train_score']), 
+                        max(gs.cv_results_['mean_test_score']),
+                        t_score,
+                        gs.best_params_,
+                        end_time - start_time)
+        
+with Pool(processes=4) as pool:
+    log('==== CELL CV ====')
+    start_time = time.time()
+    pool.map(run_trial, range(trials))
+    dur = str(datetime.timedelta(seconds=(time.time() - start_time)))
+    print('==== CV Complete ====')
+    log([f'Total Time: {dur}', '==== CV Complete ===='])
